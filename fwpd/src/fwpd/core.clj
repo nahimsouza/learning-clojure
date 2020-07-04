fd2fdafa3e563ff89bfd62c7d309b22642dddc9b
Nahim Souza <nahimsouza@outlook.com>
2020-07-04 17:32:21 -0300

    [fwpd] Finishing exercises of chap 4

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -53,3 +53,57 @@
 
 ; calling the functions to filter the data
 (glitter-filter 3 (mapify (parse (slurp filename))))
+
+;; EXERCISES
+
+; return only the filtered names (not the whole record)
+(defn glitter-filter-name
+  "Filters out those with a :glitter-index less than the provided minimum-glitter"
+  [minimum-glitter records]
+  (map :name (filter #(>= (:glitter-index %) minimum-glitter) records)))
+
+(glitter-filter-name 3 (mapify (parse (slurp filename))))
+
+
+; append new suspect to the list
+(defn append-suspect
+  "Append a new suspect to the list"
+  [suspect-list suspect]
+  (conj suspect-list suspect))
+
+(def suspect-list (mapify (parse (slurp filename))))
+
+(def new-suspect {:name "Teddy Bear"
+                  :glitter-index 2})
+
+(append-suspect suspect-list new-suspect)
+
+; function to validate record
+(defn valid-record?
+  "Returns nil if the record is invalid"
+  [record]
+  (and (:name record)
+       (:glitter-index record)
+       record))
+
+; append new suspect to the list
+(defn append-valid-suspect
+  "Append only valid suspect to the list, otherwise return nil"
+  [suspect-list suspect]
+  (and (valid-record? suspect)
+       (conj suspect-list suspect)))
+
+; (valid-record? new-suspect)
+; (append-valid-suspect suspect-list new-suspect)
+(append-valid-suspect suspect-list 2)
+
+; convert a list of maps to a CSV string
+(defn tocsv
+  "Convert list of suspects to CSV string"
+  [suspects]
+  (clojure.string/join "\n"
+    (map (fn [suspect]
+            (clojure.string/join "," (map suspect vamp-keys)))
+        suspects)))
+
+(tocsv suspect-list)
